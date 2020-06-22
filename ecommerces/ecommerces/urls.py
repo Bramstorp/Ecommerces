b5b3fb2eff517c42b6dcc1fc51ee79895095577e
Bramstorp <m.bramstorp@stofanet.dk>
2020-06-22 13:05:00 +0200

    Core Setup and templates

--- ecommerces/ecommerces/urls.py
+++ ecommerces/ecommerces/urls.py
@@ -1,5 +1,4 @@
-"""ecommerces URL Configuration
-
+"""DjangoEcommerces URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -14,8 +13,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("ecommercesCore.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
